6ef298167bb18b076f9d258cf92f467e3677db8c
Chris Abrams <cabrams@stumbleupon.com>
2013-10-29 17:11:52 -0400

    Update slug method.

--- lib.js
+++ lib.js
@@ -47,13 +47,22 @@ module.exports = helpers = {
     }
   },
 
-  slugify: function (string) {
+  slugify: function (str) {
 
-    return string
-      .toLowerCase()
-      .replace(/[^\w ]+/g,'')
-      .replace(/ +/g,'-')
-      ;
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+    
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to   = "aaaaeeeeiiiioooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '')
+      .replace(/\s+/g, '-')
+      .replace(/-+/g, '-');
+
+    return str;
 
   }
 
